3524ec9aaee27231677adeaaca045f58680839ed
Connor Bailey <cbailey9@wisc.edu>
2026-06-26 19:04:57 -0500

    Add size property and len dunder to Dataset

--- src/visselect/dataset.py
+++ src/visselect/dataset.py
@@ -18,13 +18,29 @@ class Dataset:
 
         Raises: 
             TypeError: If data is not a NumPy array
-            ValueError: If data is not 2D
+            ValueError: If data is not 2D or has no rows/columns
         """
 
         if not isinstance(data, np.ndarray):
             raise TypeError(f"Data is {type(data).__name__}, not a NumPy array")
-            
         if data.ndim != 2:
-            raise ValueError(f"Data is {data.ndim}D, not 2D.")
-        
+            raise ValueError(f"Data is {data.ndim}D, not 2D")
+        if data.shape[0] == 0:
+            raise ValueError("Data has no items")
+        if data.shape[1] == 0:
+            raise ValueError("Data has no features")
+
         self._root = data
+
+    @property
+    def size(self) -> tuple[int, int]:
+        """
+        The tuple (rows, columns) of the size of the dataset
+        """
+        return self._root.shape
+
+    def __len__(self) -> int:
+        """
+        Returns the number of rows in the dataset
+        """
+        return self.size[0]
